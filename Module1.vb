31acd71770f6bd2e4676fa02a2de220dc9594542
Hongjiang Zhang <honzhan@microsoft.com>
2019-12-13 11:41:02 +0800

    Demo project for how to use WS in autotransport

---
+++ Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.AspNet.SignalR.Client
+
+Module Module1
+
+    Sub Main()
+        Dim hubProxy As IHubProxy
+        Dim connection As HubConnection
+        connection = New HubConnection("http://localhost:14162")
+        hubProxy = connection.CreateHubProxy("SignalRBench")
+        connection.Start().Wait(New TimeSpan(0, 0, 110))
+    End Sub
+
+End Module
